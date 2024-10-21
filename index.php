4eb729ef3b97d7f32d5329381fdb4ca30277aeb2
Lukas Dizy <lukas.dizy@etud.univ-pau.fr>
2024-10-21 16:59:06 +0200

    Modification couleur page + cartes véhcules + navbar

--- index.php
+++ index.php
@@ -10,8 +10,20 @@
     <script src='node_modules/bootstrap/dist/js/bootstrap.bundle.js'></script>
     <title>Page d'accueil</title> <!-- MODIFIER TITRE -->
 </head>
-<body class="container">
-    
+<body class="container" style="background-color: #202020;">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">AcheterVehicule</a>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Accueil</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">Véhicules</a>
+            </li> <!-- Eventuellement un menu pour mettre tous les véhicules -->
+        </ul>
+
     <?php
         // Connexion à la base de données
         include("./connexionBDD.php");
@@ -24,29 +36,43 @@
             exit();
         }
 
+        // Navbar : connexion, déconnexion, back-office
+        echo '<ul class="navbar-nav ms-auto mb-2 mb-lg-0">';
         if (isset($_SESSION['login'])) {
             if ($_SESSION['role'] == 'admin') {
-                echo '<a href="./backOffice.php">Back-Offic</a>';
+                echo '<li class="nav-item">
+                <a class="nav-link" href="backOffice.php">Back-office</a>
+                </li>';
             }
-            echo '<a href="./logout.php">Déconnexion</a>';
+            echo '<li class="nav-item">
+            <a class="nav-link" href="logout.php">Déconnexion</a>
+            </li>';
         }
         else {
-            echo '<a href="./login.php">Connexion</a>';
+            echo '<li class="nav-item">
+            <a class="nav-link" href="login.php">Connexion</a>
+            </li>';
         }
+        echo '</ul>
+        </div>
+        </div>
+        </nav>';
 
-        
-        // Test BDD
+        // Affichage cartes véhicules
         echo "<div class='container text-center'>";
         echo '<div class="row row-cols-3">';
         while ($donnees=mysqli_fetch_assoc($result)) {
             echo '<div class="col">';
-                echo '<img src="vignette.php?nom='.$donnees["chemin_Vignette"].'&largeur=256&hauteur=144" alt='.$donnees["modele"].'>';
-                echo '<h2>'.$donnees["modele"].'</h2>';
-                echo '<h5>Prix : '.$donnees["prix"].'$</h2>';
-                echo '<a>'.$donnees["description"].'</a>';
-                $_SESSION['role'] = 0;
-                
+            echo '<div class="card" style="width: 18rem;" data-bs-theme="dark">';
+            echo '<img src="vignette.php?nom='.$donnees["chemin_Vignette"].'&largeur=256&hauteur=144" class="card-img-top" alt="'.$donnees["modele"].'">';
+            echo '<div class="card-body">';
+            echo '<strong><p class="card-text">'.$donnees["modele"].'</p></strong>';
             echo '</div>';
+            echo '</div>';
+            echo '</div>';
+                // echo '<h2>'.$donnees["modele"].'</h2>';
+                // echo '<h5>Prix : '.$donnees["prix"].'$</h2>';
+                // echo '<a>'.$donnees["description"].'</a>';
         }
         echo "</div>";
         echo "</div>";
