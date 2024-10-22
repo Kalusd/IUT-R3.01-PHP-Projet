3bb8fa85ccc9dff64f5b8479cd4761defbb6e55c
Lukas Dizy <lukas.dizy@etud.univ-pau.fr>
2024-10-22 09:15:45 +0200

    Modification connexion à la bdd encodage correct

--- connexionBDD_exemple.php
+++ connexionBDD_exemple.php
@@ -5,4 +5,5 @@
     $HOST = "lakartxela.iutbayonne.univ-pau.fr";
 
     $link = mysqli_connect($HOST,$USER,$PASSWORD,$DATABASE) or "Impossible de se connecter à la base de données";
+    mysqli_set_charset($link, "utf8");
 ?>
