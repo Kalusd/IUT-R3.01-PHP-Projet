7e7d651f96c21bdd895c110f54115cf9fce1c2fa
Lukas Dizy <lukas.dizy@etud.univ-pau.fr>
2024-10-22 23:44:07 +0200

    Modification des marges supérieures des cartes

--- vehicules.php
+++ vehicules.php
@@ -65,12 +65,14 @@
         echo '<div class="row row-cols-3">';
         while ($donnees=mysqli_fetch_assoc($result)) {
             echo '<div class="col">';
+            echo '<div class="mt-2">';
             echo '<a class="card" style="width: 18rem;" data-bs-theme="dark" href="vehicule.php?modele='.$donnees["modele"].'">';
             echo '<img src="vignette.php?nom='.$donnees["chemin_Vignette"].'&largeur=256&hauteur=144" class="card-img-top" alt="'.$donnees["modele"].'">';
             echo '<div class="card-body">';
             echo '<strong><p class="card-text">'.$donnees["modele"].'</p></strong>';
             echo '</div>';
             echo '</a>';
+            echo '</div>';
             echo '</div>';
         }
         echo "</div>";
