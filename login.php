4bf42fc935e916b9f26e0991364e7c1a2458280f
Lukas Dizy <lukas.dizy@etud.univ-pau.fr>
2024-10-23 18:58:38 +0200

    Modification du titre de la page

--- login.php
+++ login.php
@@ -41,7 +41,7 @@
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
                 <link rel="stylesheet" type="text/css" href="node_modules/bootstrap/dist/css/bootstrap.css">
                 <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.js"></script>
-                <title>Document</title>
+                <title>Connexion - AcheterVehicule</title>
             </head>
             <body class="container" style="background-color: #202020;">
                 <nav class="navbar navbar-expand-lg bg-body-tertiary rounded-bottom" data-bs-theme="dark">
